19f13d23a000bc9bca5148b43ad44bb08ffb786e
eliJI <elijahingr@gmail.com>
2022-09-01 11:04:03 -0400

    formatted

--- challenge_180/src/main.rs
+++ challenge_180/src/main.rs
@@ -8,17 +8,18 @@ fn first_unique(s: &String) {
     println!("{s}");
     let mut occurance_map = HashMap::new();
     for c in s.chars() {
-        match occurance_map.insert(c, 1){
-            Some(x) => {occurance_map.insert(c, x+1);},
+        match occurance_map.insert(c, 1) {
+            Some(x) => {occurance_map.insert(c, x + 1);}
             None         => {}
         }
     }
 
     for c in s.chars() {
         match occurance_map.get(&c) {
-            Some(x) => {if x == &1 {println!("{} is the first unique character!",&c);return ;}},
-            None => (),
+            Some(x) => {
+                if x == &1 {println!("{} is the first unique character!", &c);return;}}
+            None          => (),
         }
     }
-    println!("{:?}",occurance_map);
+    println!("{:?}", occurance_map);
 }
